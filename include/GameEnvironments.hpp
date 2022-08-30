0ac302a4e0889a92b5cba6fa8fc8aa166db2a2b7
MaskedPaladin <carjien@gmail.com>
2022-08-31 00:25:05 +0200

    Added code to functions of the GameEnvironments header forward declarations in the cpp

--- include/GameEnvironments.hpp
+++ include/GameEnvironments.hpp
@@ -27,5 +27,16 @@ namespace GameEnvironments
 			~Table();
 			Table(std::string name);
 			Table(std::string name, GameObjects::Deck deck);
+			void setName(std::string name);
+			std::string getName();
+			void setDeck(GameObjects::Deck deck);
+			GameObjects::Deck getDeck();
+			void setPlayers(std::vector<Entities::Player> players);
+			std::vector<Entities::Player> getPlayers();
+			void setCards(std::vector<GameObjects::Card> cards);
+			std::vector<GameObjects::Card> getCards();
+			void setPot(int pot);
+			int getPot();
+
 	};
 }
--- src/GameEnvironments.cpp
+++ src/GameEnvironments.cpp
@@ -2,3 +2,17 @@
 
 using namespace GameEnvironments;
 
+Table::Table(){}
+Table::~Table(){}
+Table::Table(std::string name){ this->name = name; }
+Table::Table(std::string name, GameObjects::Deck deck){ this->name = name; this->deck = deck; }
+void Table::setName(std::string name){ this->name = name; }
+std::string Table::getName(){ return this->name; }
+void Table::setDeck(GameObjects::Deck deck){ this->deck = deck; }
+GameObjects::Deck Table::getDeck(){ return this->deck; }
+void Table::setPlayers(std::vector<Entities::Player> players){ this->players = players; }
+std::vector<Entities::Player> Table::getPlayers(){ return this->players; }
+void Table::setCards(std::vector<GameObjects::Card> cards){ this->cards = cards; }
+std::vector<GameObjects::Card> Table::getCards(){ return this->cards; }
+void Table::setPot(int pot){ this->pot = pot; }
+int Table::getPot(){ return this->pot; }
