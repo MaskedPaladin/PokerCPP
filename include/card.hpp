cac104d78c900a949bd11ca2b789d596f4876d5a
MaskedPaladin <carjien@gmail.com>
2022-08-30 16:23:37 +0200

    Initial commit

---
+++ include/card.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class Card
+{
+	private:
+		int value;
+		std::string repr;
+		std::string suit;
+	public:
+		Card();
+		~Card();
+		Card(int value, std::string repr, std::string suit);
+		void setValue(int value);
+		int getValue();
+		void setRepr(std::string repr);
+		std::string getRepr();
+		void setSuit(std::string suit);
+		std::string getSuit();
+		void show();
+};
---
+++ src/card.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include "../include/card.hpp"
+
+
+Card::Card(){}
+Card::Card(int value, std::string repr, std::string suit){ this->value = value; this->repr = repr; this->suit = suit; }
+void Card::setValue(int value){ this->value = value; }
+int Card::getValue(){ return this->value; }
+void Card::setRepr(std::string repr){ this->repr = repr; }
+std::string Card::getRepr(){ return this->repr; }
+void Card::setSuit(std::string suit) { this->suit = suit; }
+std::string Card::getSuit(){ return this->suit; }
+void Card::show(){ std::cout << std::to_string(this->value) << "," << this->repr << "," << this->suit << std::endl; }
